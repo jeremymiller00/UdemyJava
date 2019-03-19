a44dd798577eb31587c09b7ecf464657c9455846
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-19 14:23:23 -0700

    separate classes

--- src/com/clarivate/robot/App.java
+++ src/com/clarivate/robot/App.java
@@ -2,9 +2,43 @@ package com.clarivate.robot;
 
 public class App {
     public static void main(String[] args) {
-        Robot R2D2 = new Robot();
-        System.out.println((R2D2.name));
-        System.out.println((R2D2.age));
+//        Robot R2D2 = new Robot();
+//        System.out.println((R2D2.name));
+//        System.out.println((R2D2.age));
+
+//        Robot C3PO = new Robot("C3PO");
+//        System.out.println(C3PO.name);
+
+//        Robot R5D4 = new Robot("R5D4", 42);
+//        System.out.println(R5D4.name, R5D4.age);
+
+//        Robot.description = "I am a robot!"; // class variables, every instance has the same variable
+
+        System.out.println("Initial Count: "+Robot.count);
+        System.out.println(Robot.description);
+
+        Robot robo1 = new Robot();
+        Robot robo2 = new Robot();
+        Robot robo3 = new Robot();
+
+        robo1.name = "Yup"; // instance variables
+        robo2.name = "Nope"; // every instance gets in own copy
+
+//        System.out.println(robo1.name);
+//        System.out.println(robo2.name);
+
+        robo1.showName(); // instance methods
+        robo2.showName();
+
+//        robo1.showInfo();
+//        robo2.showInfo();
+
+        System.out.println(robo1.description);
+        System.out.println(Math.PI);
+        System.out.println(Robot.luckyNumber);
+        System.out.println("Current Count: "+Robot.count);
+
+
 
     }
 }
--- src/com/clarivate/robot/Robot.java
+++ src/com/clarivate/robot/Robot.java
@@ -3,18 +3,43 @@ package com.clarivate.robot;
 class Robot {
     public String name;
     public Integer age;
+    private static String description;
+    public final static int luckyNumber = 7;
+    public static int count = 0;
+    private int id;
+
+    this.description = "I am a robot!"; // class variables, every instance has the same variable
+
 
     public Robot() { // Constructor
+        this("Karat", 67);
         System.out.println("Constructor running...");
-        age = 99;
-        name = "R2D2";
+        count++;
+//        age = 99;
+//        name = "R2D2";
+    }
 
-    public Robot(String name) {
-        System.out.println("2nd constructor...");
+    public Robot(String name) { // Constructor
+        System.out.println("2nd Constructor running...");
         this.name=name;
+    }
+
+    public Robot(String name, int age) { // Constructor which takes a String
+        System.out.println("3rd constructor...");
+        this.name=name;
+        this.age=age;
         }
 
+    public void showName() {
+        System.out.println("Hello! My name is " + name + ". " + description);
     }
-}
+
+    public static void showInfo() {
+        System.out.println(description );
+    }
+
+
+
+    }
 
 
