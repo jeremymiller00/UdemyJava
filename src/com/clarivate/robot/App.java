9fe364afe4855d8880206ea1f024417bda8c7f5b
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-19 11:30:30 -0700

    build constructors

---
+++ src/com/clarivate/robot/App.java
@@ -0,0 +1,10 @@
+package com.clarivate.robot;
+
+public class App {
+    public static void main(String[] args) {
+        Robot R2D2 = new Robot();
+        System.out.println((R2D2.name));
+        System.out.println((R2D2.age));
+
+    }
+}
--- src/com/clarivate/robot/Robot.java
+++ src/com/clarivate/robot/Robot.java
@@ -1,8 +1,19 @@
 package com.clarivate.robot;
 
-public class Robot {
-    public Robot() {
+class Robot {
+    public String name;
+    public Integer age;
+
+    public Robot() { // Constructor
         System.out.println("Constructor running...");
+        age = 99;
+        name = "R2D2";
+
+    public Robot(String name) {
+        System.out.println("2nd constructor...");
+        this.name=name;
+        }
+
     }
 }
 
