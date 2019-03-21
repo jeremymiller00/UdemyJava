f39038f40217b2cc0bd38f7ea4cd2b572bddfd1e
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-21 13:36:27 -0700

    start basiscs class

---
+++ basics/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Starting main method...");
+
+    Variables var = new Variables();
+    System.out.println(var.myNumber);
+  }
+}
---
+++ basics/src/Variables.java
@@ -0,0 +1,5 @@
+public class Variables {
+  int myNumber = 99;
+  short mtShort = 9876;
+
+}
